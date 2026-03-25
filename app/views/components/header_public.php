77e2e56a31ee93ad08ed90840881e284b6935984
Esteban Bedoya <est.bed@icloud.com>
2026-03-25 15:57:40 -0500

    V1.13_P se crean y asignan los diferentes headers segun el logueo

---
+++ app/views/components/header_public.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom border-neon">
+  <div class="container">
+    <a class="navbar-brand fw-bold" href="index.php">
+      <i class="bi bi-hospital"></i> Medicar<span class="text-neon">Flow</span>
+    </a>
+    <span class="navbar-text ms-auto d-none d-sm-block">
+      Módulo de Nómina v1.0
+    </span>
+  </div>
+</nav>
--- app/views/oper/nomina.php
+++ app/views/oper/nomina.php
@@ -1,14 +1,27 @@
-<?php $activePage = 'nomina';?>
+<?php $activePage = 'nomina'; ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nomina</title>
+    <title>Nomina - Operario</title>
 </head>
+
 <body>
+
+    <?php
+    // Determinar qué header cargar según el rol guardado en el login
+    if ($_SESSION['rol'] == 1) {
+        include __DIR__ . '/../components/header_administrador.php';
+    } else {
+        include __DIR__ . '/../components/header_operario.php';
+    }
+    ?>
+
+
     <h1>Logueo como operador</h1>
 </body>
-</html>
 
+</html>
